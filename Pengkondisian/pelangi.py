06e8557cb9057236abfd542212bbdd70acabe081
= <=>
2024-10-08 09:16:50 +0700

    Pengkondisian

---
+++ Pengkondisian/pelangi.py
@@ -0,0 +1,38 @@
+#bikin warna pelangi
+# created by Arsa
+# 8-10-2024
+
+print('\n')
+print('='*30)
+print('Warna Pelangi')
+print('='*30)
+
+while True:
+ warna = str(input('\nMasukan warna : '))
+ 
+ if warna == 'merah':
+     print('Red')
+ elif warna == 'kuning':
+     print('Yellow')
+ elif warna == 'biru':
+     print('Blue')
+ elif warna == 'hijau':
+     print('Green')
+ elif warna == 'merah muda':
+     print('Pink')
+ elif warna == 'biru langit':
+     print('SkyBlue')
+ elif warna == 'abu abu':
+     print('Grey')
+ elif warna == 'hitam':
+     print('Black')
+ elif warna == 'putih':
+     print('White')
+ elif warna == 'coklat':
+     print('Brown')
+ elif warna == 'jingga':
+     print('Orange')
+ elif warna == 'ungu':
+     print('Purple')
+ else:
+     print('Ini bukan warna')
