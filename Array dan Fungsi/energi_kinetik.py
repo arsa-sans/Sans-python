e6c51c4ed02d55e78c377d1b9c53153d53846b57
= <=>
2024-10-21 19:07:08 +0700

    Array

---
+++ Array dan Fungsi/energi_kinetik.py
@@ -0,0 +1,15 @@
+# Menghitung energi kinetik
+# created by Arsa
+# 8-10-2024
+
+print('\n')
+print('='*30)
+print('Menghitung energi kinetik')
+print('='*30)
+
+massa = int(input('Masukan massa : '))
+v = int(input('Masukan kecepatan : '))
+
+ek = 1/2 * massa * v * v
+
+print('Energi kinetiknya adalah ', ek)
