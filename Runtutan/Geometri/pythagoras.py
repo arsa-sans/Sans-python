a877d89c537ad000467a944d4e65410396e3ff4c
= <=>
2024-10-21 16:38:58 +0700

    Pythagoras

---
+++ Runtutan/Geometri/pythagoras.py
@@ -0,0 +1,16 @@
+# rumus pythagoras
+# created by Arsa
+# 5-10-2024
+
+print('\n')
+print('='*30)
+print('Rumus pythagoras')
+print('='*30)
+
+a = int(input('Masukan sudut a : '))
+b = int(input('Masukan sudut b : '))
+
+c = a**2 + b**2
+
+print('\nKemiringan adalah : ', c)
+print()
