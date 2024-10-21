ed8092bfd0a59d9553d26e06ff1b1e794396e905
= <=>
2024-10-21 18:56:02 +0700

    Array

---
+++ Array/bulan.py
@@ -0,0 +1,28 @@
+# Menampilkan bulan dengan array
+# Created by arsa
+# 18-10-2024
+
+bulan = ['januari',
+         'februari',
+         'maret',
+         'april',
+         'mei',
+         'juni',
+         'juli',
+         'agustus',
+         'september',
+         'oktober',
+         'november',
+         'desember'
+]
+print(bulan[2])
+print(bulan[9])
+bulan[7] = 'august'
+bulan[0] = 'january'
+bulan.append('muharam')
+
+i = 1
+for j in bulan:
+    print('Bulan ke', i, j)
+    i = i+1
+
