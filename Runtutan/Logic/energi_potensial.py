89307d4528ef1122e00604cda80fd3883be2942e
= <=>
2024-10-21 17:18:31 +0700

    Runtutan logic

---
+++ Runtutan/Logic/energi_potensial.py
@@ -0,0 +1,16 @@
+# Rumus energi potensial
+# created by Arsa
+# 21-10-2024
+
+print('\n')
+print('='*30)
+print('Rumus energi potensial')
+print('='*30)
+
+m = int(input('Masukan massa benda : '))
+g = 10
+h = int(input('Masukan ketinggian benda : '))
+
+ep = m * g * h
+
+print('Energi potensialnya adalah : ', ep)
