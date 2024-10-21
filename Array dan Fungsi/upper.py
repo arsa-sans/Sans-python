a377670c0d2751087c7a18f1bf1eb47e6997b345
= <=>
2024-10-21 17:43:13 +0700

    Fungsi Upper

---
+++ Array dan Fungsi/upper.py
@@ -0,0 +1,12 @@
+# Fungsi upper
+# created by Arsa
+# 21-10-2024
+
+print('\n')
+print('='*30)
+print('Fungsi upper')
+print('='*30)
+
+teks = input('Masukan teks : ')
+teks_kecil = teks.upper()
+print(teks_kecil)
