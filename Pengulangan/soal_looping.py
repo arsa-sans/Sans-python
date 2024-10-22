44caf36779fdb68f62edbcab377990ccc9e9579d
= <=>
2024-10-22 13:43:49 +0700

    looping

--- Pengulangan/soal_looping.py
+++ Pengulangan/soal_looping.py
@@ -20,42 +20,6 @@ print('\n')
 # # iterasi
 #     i = i + 1
 
-i = 1 
-while i < 4:
-    print(i, '-Hello ', end='')
-    i = i + 1
-print( )
-
-j = 3
-while j > 0:
-    print(j, '-Hello ', end='')
-    j = j - 1
-print( )
-
-k = 1
-while k < 6:
-    print(k, '-Hello ', end='')
-    k = k + 2
-print( )
-
-l = 0
-while l < 7:
-    print(l, '-Hello ', end='')
-    l = (l + 1) * 2
-print( )
-
-m = 10
-while m < 101:
-    print(m, end=' ')
-    m = m + 10
-print( )
-
-n = 2000
-while n < 2006:
-    print(n, end=' ')
-    n = n + 1
-print( )
-
 BARIS = 5
 for a in range(BARIS):
     for b in range(BARIS-a):
